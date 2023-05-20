f5a0d02088436a72292449428b313492aff61181
ali fareeq noori <65049788+alifareeq77@users.noreply.github.com>
2023-05-20 22:14:29 +0300

    db is now working

--- projectWeb/urls.py
+++ projectWeb/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home.html", views.home, name="home"),  #a path for route of homepage
-    ]
+    path("home", views.home, name="home"),  # a path for route of homepage
+]
